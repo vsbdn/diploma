85fb1ab83bc2cf136757f2f80506ef98c84765ed
admin <v.boldurean@gmail.com>
2020-06-02 21:30:54 +0100

    Adedd objects for footer

--- src/js/main.js
+++ src/js/main.js
@@ -26,4 +26,17 @@ $(document).ready(function () {
         },
     })
 
+    $('.select-trigger').on('click', function () {
+        $(this).parent().toggleClass('active');
+    });
+
+    $('.select-list li').on('click', function () {
+        const day = $(this).data('day');
+        const discount = $(this).data('discount');
+        const parent = $(this).parents('.select');
+        parent.find('.select-trigger .select-trigger-days').html(`${day} Дней`);
+        parent.find('.select-trigger .select-trigger-discount').html(`-${discount}%`);
+        $(this).parents('.select').removeClass('active');
+    });
+
 });
