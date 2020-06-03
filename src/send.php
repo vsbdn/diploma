07ef0da22df6edf309859f9437e5d7a98b5ccb6c
admin <v.boldurean@gmail.com>
2020-06-03 11:52:54 +0100

    added: footer form

---
+++ src/send.php
@@ -0,0 +1,42 @@
+<?php
+
+$userName = $_POST['discount'];
+$userPhone = $_POST['userName'];
+
+// Load Composer's autoloader
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'GloYnob@gmail.com';                     // SMTP username
+    $mail->Password   = 'NH510jGoo4s';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('GloYnob@gmail.com', Василий);
+    $mail->addAddress('yesnookbye@gmail.com');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}, Его почта: ${userEmail}, Его вопрос: ${userQuestion}";
+
+    if ($mail->send()) {
+        echo "ok";
+    } else {
+        echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    }
+
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
