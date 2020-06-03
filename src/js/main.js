085c3e6c677f13c3236a4678eeb8181338599e76
admin <v.boldurean@gmail.com>
2020-06-03 17:48:19 +0100

    added: footer form

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,10 @@
 $(document).ready(function () {
-    //initialize swiper when document ready
-    var swiperOne = new Swiper ('.places__swiper-container', {
-        // Optional parameters
-        loop: true,
-        slidesPerView: 2,
-        spaceBetween: 90,
-        navigation: {
-            nextEl: '.places__swiper-button-next',
-            prevEl: '.places__swiper-button-prev',
-        },
-    })
 
-    var swiperTwo = new Swiper ('.about__swiper-container', {
-        // Optional parameters
-
-        loop: true,
-        spaceBetween: 90,
-        pagination: {
-            el: '.about__swiper-pagination',
-            type: 'fraction',
-        },
-        navigation: {
-            nextEl: '.about__swiper-button-next',
-            prevEl: '.about__swiper-button-prev',
-        },
-    })
+    $('.slider').slick();
+    $('.places__slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+    });
 
     $('.select-trigger').on('click', function () {
         $(this).parent().toggleClass('active');
